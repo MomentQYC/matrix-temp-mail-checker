968eb9c49eeb0afb4005f5bc12cefd29b567db00
MomentQYC <62551256+MomentQYC@users.noreply.github.com>
2024-11-03 06:29:39 +0800

    bump version

--- matrix-temp-mail-checker/temp_mail_checker.py
+++ matrix-temp-mail-checker/temp_mail_checker.py
@@ -1,11 +1,18 @@
 from typing import Collection, Optional, Tuple
 from synapse.spam_checker_api import RegistrationBehaviour
 from synapse.module_api import ModuleApi
+from synapse.logging.context import LoggingContext
 
 import logging
 
-logger = logging.getLogger(__name__)
+try:
+    from synapse.logging.context import current_context
+except ImportError:
+    current_context = LoggingContext.current_context
 
+
+logger = logging.getLogger("synapse.tempmail_checker")
+parent_logcontext = current_context()
 
 class TempMailChecker:
 
@@ -21,7 +28,8 @@ class TempMailChecker:
     def _load_blocked_domains(self):
         with open(self.blocked_domains_file, "r") as f:
             domains = {line.strip().lower() for line in f if line.strip()}
-        logger.info(f"Loaded {len(domains)} blocked domains.")
+        with LoggingContext(parent_context = parent_logcontext):
+            logger.info(f"Loaded {len(domains)} blocked domains.")
         return domains
 
     async def check_registration_for_spam(
@@ -34,12 +42,13 @@ class TempMailChecker:
         self.blocked_domains = self._load_blocked_domains()
 
         if email_threepid:
-            try:
-                domain: str = email_threepid["address"].split("@")[-1].lower()
-            except Exception:
-                logger.critical(f"Wrong email_threepid: {type(email_threepid)} : {email_threepid}")
-                return RegistrationBehaviour.ALLOW
-            if domain in self.blocked_domains:
-                logger.warning(f"Blocked registration attempt for email domain: {domain}")
-                return RegistrationBehaviour.DENY
+            with LoggingContext(parent_context=parent_logcontext):
+                try:
+                    domain: str = email_threepid["address"].split("@")[-1].lower()
+                except Exception:
+                    logger.critical(f"Wrong email_threepid: {type(email_threepid)} : {email_threepid}")
+                    return RegistrationBehaviour.ALLOW
+                if domain in self.blocked_domains:
+                    logger.warning(f"Blocked registration attempt for email domain: {domain}")
+                    return RegistrationBehaviour.DENY
         return RegistrationBehaviour.ALLOW
